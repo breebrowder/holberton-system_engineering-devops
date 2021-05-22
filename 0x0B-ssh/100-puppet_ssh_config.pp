e0920bdf13a2760b57b847d680072f0d8a81dc31
breezybrow <browder.bree@gmail.com>
2021-05-22 02:13:04 +0000

    Using Puppet to makes changes to Task 2-Client config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,8 @@
+# ADVANCED TASK IN 0x0B- SSH
+# CLIENT CONFIGURATION WITH PUPPET
+# Puppet is an open source configuration management tool. The Puppet software pulls its configuration from code written in a Ruby DSL
+
+file { '/etc/ssh/ssh_config':
+     content => 'PasswordAuthentication No
+     IdentifyFile ~/.ssh.holberton',
+}
