4369b784a7cc96f2cd7848b4f409558325b09dc1
breezybrow <browder.bree@gmail.com>
2021-05-22 02:28:03 +0000

    Using Puppet to make changes to Task 2-Client config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # Use puppet tool to alter config file, no password auth
 
 file { '/etc/ssh/ssh_config':
-  content => 'PasswordAuthentication No
+  content => 'PasswordAuthentication no
   IdentityFile ~/.ssh/holberton',
 }
